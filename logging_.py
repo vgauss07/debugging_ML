04c41a38adba9493003f92732ac99313122cd6ca
Jeffrey Ojuederhie <vgauss07@gmail.com>
2024-01-19 05:06:06 -0500

    update

--- logging_.py
+++ logging_.py
@@ -1,5 +1,10 @@
 import logging
 def multiplying(x:float, y:float):
     """
-    
+    param x: input variable of type float
+    param y: input variable of type float
+    return: returning multiplication of the 
+    input variables
     """
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        logging.error('Input variables are not of type float or integer!.')
